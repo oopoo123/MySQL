69d42a8af880da4027a688b63f3cf4c0514013c1
oopoo123 <oopoo123@naver.com>
2023-11-09 16:08:46 +0900

    로그인 기능 추가

--- memberJoin.py
+++ memberJoin.py
@@ -8,7 +8,7 @@ from PyQt5.QtGui import *
 
 import pymysql
 
-form_class = uic.loadUiType("ui/join.ui")[0]
+form_class = uic.loadUiType("ui/login.ui")[0]
 
 class MainWindow(QMainWindow, form_class):
     def __init__(self):
@@ -17,6 +17,9 @@ class MainWindow(QMainWindow, form_class):
         self.setWindowTitle("회원가입프로그램")
 
         self.check_btn.clicked.connect(self.idCheck)
+        self.join_btn.clicked.connect(self.join)
+        self.loginid_btn.clicked.connect(self.login)
+        self.reset_btn.clicked.connect(self.reset)
 
     def idCheck(self):
         memberid = self.memberid_edit.text()  # 회원아이디로 입력된 아이디 텍스트 가져오기
@@ -37,6 +40,59 @@ class MainWindow(QMainWindow, form_class):
 
         cur.close()
         conn.close()
+
+    def join(self):
+        memberid = self.memberid_edit.text()  # 회원아이디로 입력된 아이디 텍스트 가져오기
+
+        memberid = self.memberid_edit.text()
+        memberpw = self.memberpw_edit.text()
+        name = self.name_edit.text()
+        phone = self.phone_edit.text()
+        address = self.address_edit.text()
+        age = self.age_edit.text()
+
+        conn = pymysql.connect(host='localhost', user='root', password='12345', db='memberdb')
+
+        sql = f"INSERT INTO member VALUES ('{memberid}', '{memberpw}', '{name}', '{phone}', '{address}', {age})"
+
+        cur = conn.cursor()  # 커서 생성
+        cur.execute(sql)  # SQL문 실행
+
+        cur.close()
+        conn.commit()
+        conn.close()
+
+
+    def login(self):
+        loginid = self.loginid_edit.text()
+        loginpw = self.logpw_edit.text()
+
+        conn = pymysql.connect(host='localhost', user='root', password='12345', db='memberdb')
+
+        sql = f"SELECT * FROM member WHERE memberid='{loginid}' and memberpw='{loginpw}'"
+
+        cur = conn.cursor()  # 커서 생성
+        cur.execute(sql)  # SQL문 실행
+
+        result = cur.fetchone()
+
+        if result == None:
+            self.loginid_text.setText('로그인 실패!!!')
+        else:
+            self.loginid_text.setText('로그인 성공!!!')
+
+        cur.close()
+        conn.close()
+
+
+    def reset(self):
+        self.memberid_edit.clear()
+        self.memberpw_edit.clear()
+        self.name_edit.clear()
+        self.phone_edit.clear()
+        self.address_edit.clear()
+        self.age_edit.clear()
+
 
 
 if __name__ == '__main__':
